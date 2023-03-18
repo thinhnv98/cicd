419503a631b60e9712e0ddec3cf1d81e5cfefddd
thinhnguyen.avixo <thinh.nguyen@avixo.co>
2023-03-18 17:47:33 +0700

    edit

--- Jenkinsfile
+++ Jenkinsfile
@@ -19,7 +19,7 @@ pipeline {
         }
         stage('Deploy') {
             steps {
-                echo 'Deploying....'
+                make -C cicd run
             }
         }
     }
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello 2")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	_ = r.Run(":9090")
 }
